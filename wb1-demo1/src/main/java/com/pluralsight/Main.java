da2a1c398096778881005aeeee8ec3adec1ea534
DamGarcia <damg883@gmail.com>
2025-04-14 10:53:57 -0400

    first example of expressions using concat operator

--- wb1-demo1/src/main/java/com/pluralsight/Main.java
+++ wb1-demo1/src/main/java/com/pluralsight/Main.java
@@ -2,6 +2,18 @@ package com.pluralsight;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        String word1 = "Hello"; // typing out a literal string within double quotes
+
+        String word2 = "World!"; // a literal is the value that is given to a variable not from an expression
+
+        String greeting; // defining a variable
+
+        greeting = word1 + " " + word2 + "! "; // through an expression, we assigned the value of greeting
+        // the + symbol is an operator that is concatenating strings together
+
+
+        System.out.println(greeting);
+
     }
 }
